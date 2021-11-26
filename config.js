1c1ddbdce7af3e7a8063e3838ed71e93842a2c20
vynekmnggggg <95044341+vynekmnggggg@users.noreply.github.com>
2021-11-26 19:30:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "chấm hỏi hỏi chấm",
+    introTitle: 'hey fen',
+    introDesc: `Chắc em đang shock
+    Vì thấy điều này
+    Thoi hok seo
+    hì hiì`,
+    btnIntro: 'ấn đi fen',
+    title: 'Em seen có nen chúc sn cậu í hok nhỉ 🥰',
+    desc: 'khó nghĩ wá à ',
+    btnYes: 'Tất nhiên rồi chị iu <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Chị nhìn dễ thương nhỉ <3',
+    btnReply: 'hí <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
